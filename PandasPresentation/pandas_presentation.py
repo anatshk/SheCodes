91f89e00819e970b52ff6218d1406071c9c295fd
anatshk <anat.shk@gmail.com>
2017-11-28 18:49:32 +0200

    wip pandas_presentation

--- PandasPresentation/pandas_presentation.py
+++ PandasPresentation/pandas_presentation.py
@@ -1,2 +1,14 @@
 import pandas as pd
 
+dct = {
+    'Name': ['A', 'B', 'C', 'D', 'E'],
+    'Gender': ['Male', 'Female', 'Male', 'Male', 'Female'],
+    'DOB': [pd.Timestamp(val).date() for val in ['1-Jan-80', '10-Mar-83', '31-Mar-85', '11-Jun-80', '10-Jan-80']],
+    'Pet': ['Dog', 'Cat', 'Dog', 'Dog', None],
+    'Drivers_License': [True, False, False, False, True],
+    'Height_cm': [174, 157, 189, 168, 192]
+}
+
+df = pd.DataFrame.from_dict(dct)
+
+a = 5
---
+++ PandasPresentation/taxi_demonstration.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+taxi_path = r'C:\Users\Anat\Downloads\pandas_data\taxi.csv.bz2'
+
+df = pd.read_csv(taxi_path)
