4ba7b5754fab29aac3b4c409646f304ccd937869
anatshk <anat.shk@gmail.com>
2017-12-05 21:30:13 +0200

    todos for next

--- PandasPresentation/example_dataframe.py
+++ PandasPresentation/example_dataframe.py
@@ -8,7 +8,8 @@ import pandas as pd
 dct = {
     'Name': ['A', 'B', 'C', 'D', 'E'],
     'Gender': ['Male', 'Female', 'Male', 'Male', 'Female'],
-    'DOB': [pd.Timestamp(val).date() for val in ['1-Jan-80', '10-Mar-83', '31-Mar-85', '11-Jun-80', '10-Jan-80']],
+    'DOB': [pd.Timestamp(val).date() for val in
+            ['1-Jan-80', '10-Mar-83', '31-Mar-85', '11-Jun-80', '10-Jan-80']],
     'Pet': ['Dog', 'Cat', 'Dog', 'Dog', None],
     'Drivers_License': [True, False, False, False, True],
     'Height_cm': [174, 157, 189, 168, 192]
@@ -16,5 +17,12 @@ dct = {
 
 # Initialize data from dictionary
 df = pd.DataFrame.from_dict(dct)
+
+# TODO: show how to replace index
+# TODO: show how to access columns \ rows
+# TODO: mention immutability (copies are created unless using 'inplace')
+# TODO: create new columns
+# TODO: delete values and fill missing values
+
 
 a = 5
--- PandasPresentation/taxi_demonstration.py
+++ PandasPresentation/taxi_demonstration.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 taxi_path = r'C:\Users\Anat\Downloads\pandas_data\taxi.csv.bz2'
 
-df = pd.read_csv(taxi_path)
+df = pd.read_csv(taxi_path)  # create DataFrame from CSV
+
+# TODO: add link to full data
+# TODO: write a demonstration with plots, pivot table, sample,
+# TODO: show how to write to Excel
