904c6d42c22660f0e938e085a60a57be7f8a1bf4
anatshk <anat.shk@gmail.com>
2017-12-07 17:31:19 +0200

    TODO's for taxi demo

--- PandasPresentation/taxi_demonstration.py
+++ PandasPresentation/taxi_demonstration.py
@@ -8,6 +8,12 @@ taxi_path = r'C:\Users\Anat\Downloads\pandas_data\taxi.csv.bz2'
 
 df = pd.read_csv(taxi_path)  # create DataFrame from CSV
 
-# TODO: add link to full data
+# Full data can be found here:
+# http://www.nyc.gov/html/tlc/html/about/trip_record_data.shtml
+
+a = 5
+
+# TODO: see taxi jupyter notebook for ideas.
+# TODO: think of ways to use function in previous example
 # TODO: write a demonstration with plots, pivot table, sample,
 # TODO: show how to write to Excel
